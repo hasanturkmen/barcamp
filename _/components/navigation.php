bf24c1f6389159782a9c3bd4e5418ad4f6e8b454
Ray Villalobos <ray@planetoftheweb.com>
2012-07-13 04:57:11 -0400

    Initial Commit
    First commit of the BarCampDeland website project

---
+++ _/components/navigation.php
@@ -0,0 +1,6 @@
+<ul class="nav">
+<li><a href="/">home</a></li>
+<li><a href="/feedback/">What the heck is a BarCamp?</a></li>
+<li><a href="/feedback/">Sign Me Up</a></li>
+
+</ul>
---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Barcamp Deland -- October 6, 2012 in Downtown Deland</title>
+	<link href='http://fonts.googleapis.com/css?family=Economica|Enriqueta' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="_/css/import.css" type="" />
+	<link rel="stylesheet" href="_/css/mystyles.css" type="" />
+</head>
+<body>
+
+<!-- Navigation -->
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="#">Barcamp Deland 2012</a>
+      <div class="nav-collapse">
+        <ul class="nav">
+          <li class="active"><a href="#">Home</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+</div>
+
+<div id="content" class="container-fluid">
+	<div class="row-fluid">
+		<div class="span12">
+
+			<img id="branding" src="images/barcampdelandlogo.png" alt="Deland BarCamp 2012" />
+
+			<div class="hero-unit">
+				<h1>Free Technology Unconference</h1>
+				<p>Saturday, October 6th • 9:00 a.m. - 5 p.m.</p>
+				<p><a class="btn btn-primary btn-large">Registration available soon...</a></p>
+			</div>
+		</div>
+	</div><!-- row -->
+	<div id="info" class="row-fluid">
+		<div id="primary" class="span8">
+			<h2>What is BarCamp Deland?</h2>
+			<p>BarCamp Deland is ad-hoc unconference event centered around technology. It aims to bring people together from different backgrounds like web-design, photography, graphic design web-development and technology for cooperative learning. Born from the desire for people to share and learn in an open environment. It is an intense event with discussions, demos and interaction from attendees. Anyone with something to contribute or with the desire to learn is welcome and invited to join. When you come, be prepared to share.</p>
+	
+	
+			<h2>No spectators...only participants</h2>
+			<p>Attendees must give a demo, a session, or help with one, or otherwise volunteer/contribute in some way to support the event. All presentations are scheduled the day they happen. Prepare in advance, but come early to get a slot on the wall. The people present at the event will select the demos or presentations they want to see. Presenters are responsible for making sure that notes/slides/audio/video of their presentations are published on the web for the benefit of all and those who can’t be present.</p>
+
+			<h2>Rules of Deland BarCamp</h2>
+			<ul>
+				<li>If you want to present, you must write your topic and name in a presentation slot.</li>
+				<li>As many presentations at a time as facilities allow for.</li>
+				<li>No pre-scheduled presentations</li>
+				<li>Presentations will go on as long as they have to or until they run into another presentation slot.</li>
+			</ul>
+
+			<h2>Help us promote</h2>
+			<p>Help us promote our event by using one of the logos below! Spread the word with a mention on Twitter or any other social network.</p>
+			<p>
+			<a href="/images/barcampdeland.pdf" class="btn btn-primary">Download PDF Logo</a>
+
+
+
+			<a class="btn btn-warning" data-toggle="modal" href="#pnglogo">Get PNG Logo</a>
+
+			<div class="modal hide fade" id="pnglogo">
+				<div class="modal-body">
+					<button type="button" class="close" data-dismiss="modal">×</button>			  
+			    	<p><img src="/images/barcampdeland-hr.png" /></p>
+				</div>
+			</div>
+			</p>
+
+		</div>
+		<div id="secondary" class="span4">
+			<h2>Venue &amp; Times</h2>
+			
+			<p><iframe height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=110+Indiana+Ave,+DeLand,+FL&amp;sll=29.028979,-81.305417&amp;sspn=0.023302,0.021393&amp;ie=UTF8&amp;hq=&amp;hnear=110+E+Indiana+Ave,+DeLand,+Florida+32724&amp;t=m&amp;z=17&amp;ll=29.028982,-81.303298&amp;output=embed"></iframe></p>
+			
+			<p>The event will be held Saturday, October 6th in Downtown Deland from 9:30 a.m.–6 p.m. and we hope to be able to host an after party at one or more local venues.</p>
+
+
+			<h2>Proposed Schedule</h2>
+			<p>We're still in the planning stages for this project, but here's a proposed schedule for the event.</p>
+
+			<table class="table table-striped table-condensed">
+			  <tbody>
+			    <tr>
+			      <td>9:00 a.m</td>
+			      <td>Doors open, people can sign up for talks on the whiteboard</td>
+			    </tr>
+			    <tr>
+			      <td>10:00 a.m</td>
+			      <td>Doors open, people can sign up for talks on the whiteboard</td>
+			    </tr>
+			    <tr>
+			      <td>12:00 p.m</td>
+			      <td>Lunch is served (if available)</td>
+			    </tr>
+			    <tr>
+			      <td>1:00 p.m.</td>
+			      <td>Talks resume</td>
+			    </tr>
+			    <tr>
+			      <td>5:00 p.m</td>
+			      <td>Talks ends</td>
+			    </tr>
+			    <tr>
+			      <td>6:00 p.m</td>
+			      <td>After party (TBD)</td>
+			    </tr>
+			  </tbody>
+			</table>
+
+			<h2>Parking</h2>
+			<a class="btn btn-warning btn-large" data-toggle="modal" href="#parking" id="parkingbtn" >Parking</a>
+			<p>Parking is available in many places throughout the Downtown Deland area.</p>
+
+			<div class="modal hide fade" id="parking">
+			  <div class="modal-body">
+				<button type="button" class="close" data-dismiss="modal">×</button>			  
+			    <p><img src="/images/parking-downtowndeland.png" /></p>
+			  </div>
+			</div>
+
+
+			<h2>Sponsors</h2>
+			<p>We hope to provide free t-shirts and lunch to all attendees, but can only do so with help from sponsors.</p>
+		</div>
+	</div>
+
+</div><!-- container -->	
+			
+			
+<footer>
+	<div class="navbar-bottom fixed">
+	  <div class="navbar-inner">
+	    <div class="container">
+		<div class="nav-collapse">
+			<p></p>
+		</div><!-- nav-collapse -->
+		</div><!-- container -->
+	  </div><!-- navbar-inner -->
+	</div><!-- navbar -->
+</footer>
+
+    <script src="../_/js/import.js" type="text/javascript"></script>
+    <script src="../_/js/myscript.js" type="text/javascript"></script>    
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
